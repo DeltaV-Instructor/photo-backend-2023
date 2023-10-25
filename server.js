6b9cc5931c46789fefd1a0523ca2070846b16e9c
tektechnologies <info@tektechnologies.com>
2023-10-25 11:56:22 -0500

    comments for server build

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 'use strict';
+
 console.log('server running');
+
+
+// set up our requires
+
+
+// add some routes to call the unsplash application
+
+// set up server to listen on a prompt
+
+
+// create class to construct objects for us to send back to front end 
+
+//some error handling
+
+//handling routes that exsist
