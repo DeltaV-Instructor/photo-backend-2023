e29317a9ff9d0b5c58d8a792977c6790e9468956
tektechnologies <info@tektechnologies.com>
2023-10-27 10:23:35 -0500

    weatherHandler is connected

--- modules/weather.js
+++ modules/weather.js
@@ -9,14 +9,26 @@ console.log('weather js file connected');
 
 function getWeather(){
   console.log('in the get weather');
+  //is going to call our api to get a weather forecast
+  //set the search into cache
+  //is going send the axios response from the api to function called parseWeather
+
 }
 
 
 
 
+function parseWeather(){
+  //we are going to loop over the api data and send it to our weather class for instantiation for our front to render
+}
 
 
 
+class Weather{
+  constructor(day){
+
+  }
+}
 
 
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 const getPhotos = require('./modules/photo');
 const weather = require('./modules/weather.js');
-console.log('is this our function',weather);
+// console.log('is this our function',weather);
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5005;
@@ -19,7 +19,16 @@ app.get('/', (request, response)=>{
 
 
 app.get('/photos', getPhotos);
+app.get('/weather', weatherHandler);
 
+
+
+
+function weatherHandler(request, response){
+  //take in our front end values and send them to our weather.js and then get back some weather to send to the front end
+  console.log('request.query', request.query.lat, request.query.lon, request.query.locationName);
+  response.status(206).send('WIP');
+}
 
 
 
